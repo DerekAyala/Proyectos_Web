84f3fd054df88120a7c2771aca05a69ff14cd2fd
Derek Ayala <derek.ayala12345@hotmail.com>
2021-10-11 18:17:03 -0500

    actualizacion al proyecto de festival de musica

--- FestivalMusica/src/js/app.js
+++ FestivalMusica/src/js/app.js
@@ -1,37 +1,95 @@
 document.addEventListener('DOMContentLoaded', function(){
-    scrollNav();
-    navegacionFija();
+    iniciarApp();
 });
 
-function navegacionFija(){
-
-    const bara = document.querySelector('.header');
-    //registrar el intersection Observer
-    const observer = new IntersectionObserver( function(entries){
-        if(entries[0].isIntersecting){
-            bara.classList.remove('fijo')
-        }else{
-            bara.classList.add('fijo')
-        }
-    })
-    //elemento a observar
-    observer.observe(document.querySelector('.sobre-festival'));
-
+function iniciarApp() {
+    navegacionFija();
+    crearGaleria();
+    scrollNav();
 }
 
-function scrollNav(){
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+
+    window.addEventListener('scroll', function() {
+        if( sobreFestival.getBoundingClientRect().bottom < 0  ) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+
+
+function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
 
-    enlaces.forEach(function(enlace){
-        enlace.addEventListener('click', function(e){
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
             e.preventDefault();
-            // console.log(e.target.attributes.href.value);
 
-    
-            const seccion = document.querySelector(e.target.attributes.href.value);
-            seccion.scrollIntoView({
-                behavior:'smooth'
-            });
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth"});
         });
     });
+}
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i = 1; i <= 12; i++ ) {
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `;
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}  
+
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+    `;
+
+    // Crea el Overlay con la imagen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    // Añadirlo al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
