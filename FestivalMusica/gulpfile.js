d548fffb83a540aea230ae0bafca3988e5bedd76
Derek Ayala <derek.ayala12345@hotmail.com>
2021-10-11 18:01:07 -0500

    Actualización de dependencias

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,91 +1,82 @@
-const { series , parallel , src , dest , watch} = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
-const sass = require('gulp-sass');
-const imagemin = require('gulp-imagemin');
-const notify = require('gulp-notify');
-const webp = require('gulp-webp');
-const concat = require('gulp-concat');
- //utilidades css
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 
-//utilidades js
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// Javascript
 const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
 
-const paths = {
-    imagenes: './src/img/**/*',
-    scss: "./src/scss/**/*.scss",
-    js: 'src/js/**/*.js'
-}
-function css(){
-    return src(paths.scss)
-        .pipe( sourcemaps.init() )
-        .pipe( sass({
-            // outputStyle: 'expanded' //compresed
-        }) )
-        .pipe( postcss( [autoprefixer(), cssnano()] ))
-        .pipe( sourcemaps.write('.') )
-        .pipe( dest('./build/css') )
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
+    done();
 }
 
-function javascript(){
-    return src(paths.js)
-        .pipe( sourcemaps.init() )
-        .pipe( concat('bundle.js'))
-        .pipe(terser() )
-        .pipe( sourcemaps.write('.') )
-        .pipe( rename({ suffix:'.min'}))
-        .pipe( dest('./build/js') )
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-function imagenes(){
-    return src(paths.imagenes)
-        .pipe( imagemin() )
-        .pipe( dest( './build/img' ))
-        .pipe( notify({ message: 'Imagen Minificada'}) );
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-function versionWebp(){
-    return src(paths.imagenes)
-        .pipe(webp())
-        .pipe( dest( './build/img'))
-        .pipe(notify({ message: 'Version Wep lista'}));
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-function watchArchivos(){
-    watch(paths.scss, css); // * = carpeta actual  **/* = varias carpetas
-    watch(paths.js, javascript)
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
 }
 
-
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
+}
 
 exports.css = css;
-exports.watchArchivos = watchArchivos;
+exports.js = javascript;
 exports.imagenes = imagenes;
-
-exports.default = series( css , javascript, imagenes , versionWebp ,watchArchivos);
-/*
-function hola(done){
-    console.log('Hola mundo en Gulp');
-    done()
-}
-
-function javascript(done){
-    console.log('compilando javascript');
-    done()
-}
-
-function css(done){
-    console.log('compilando css');
-    done()
-}
-
-exports.javascript = javascript;
-exports.hola = hola;
-
-exports.default = series(hola , javascript , css);
-exports.default = parallel(hola , javascript , css);
-*/
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev) ;
